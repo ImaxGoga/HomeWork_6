b21e3487d8c929f67b46a50781599959a71a8db3
ImaxGoga <45959194+ImaxGoga@users.noreply.github.com>
2022-09-19 12:36:49 +0300

    First done

---
+++ 1_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void foo()
+{
+    int len, res = 0;
+    Console.WriteLine("Сколько чисел Вы хотите ввести?");
+    len = Int32.Parse(Console.ReadLine());
+    double[] mass = new double[len];
+    for (int i = 0; i < len; i++)
+    {
+        Console.WriteLine("Введите " + i + "число: ");
+        mass[i] = double.Parse(Console.ReadLine());
+        if(mass[i] > 0) res++;
+    }
+    Console.WriteLine();
+    Console.Write("Ваш массив [");
+    for (int i = 0; i < len; i++)
+    {
+        if(i == len - 1)
+        Console.WriteLine(mass[i] + "]");
+        else Console.Write(mass[i] + ", ");
+    }
+    Console.WriteLine("Количество чисел больше 0: " + res);
+}
+
+foo();
